924f2370d8d2e1e122dc0be7a5c645af2a47bd6e
andreasmb <andreasmb@users.noreply.github.com>
2024-05-18 23:25:37 +0000

    deploy: 16ee0309af8807ace6ed9a918f4182e727acbcfc

---
+++ js/env.js
@@ -0,0 +1,2 @@
+window.env = { AIRTABLE_API_KEY: 'patxPiHh1pcDU4oKe.3676b903b56bff1dd7537271873d5b6daf01f850b12cf991fbb81f8d17fa133d', AIRTABLE_BASE_ID: 'appKtQz6NKGGJRb8b' };
+console.log('Environment Variables:', window.env);
